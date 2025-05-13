cbeb84fefef934c236e1456b940c3e6e2d3828c4
SimoneMarchi97 <simonemarchi27011997@gmail.com>
2025-05-13 11:07:49 +0200

    file streamlit

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 def main ():
-    st.title("La bottega del gusta")
+    st.title("La bottega del gusto")
 
     st.markdown(
     """
     <style>
     body {
         background-color: #000000;
-        color: white; /* Opzionale: imposta il colore del testo per contrasto */
+        color: #FFFFFF;                       /* Opzionale: imposta il colore del testo per contrasto */
     }
     .stApp {
-        background-color: #000000; /* Assicura che anche l'area principale sia nera */
+        background-color: #000000;            /* Assicura che anche l'area principale sia nera */
     }
     </style>
     """,
