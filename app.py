06a668c342ab112661d5900046cd488f205552a8
SimoneMarchi97 <simonemarchi27011997@gmail.com>
2025-05-13 11:03:47 +0200

    file streamlit

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 import streamlit as st
 
 def main ():
-    st.title("PRIMA app")
+    st.title("La bottega del gusta")
+
+    st.markdown(
+    """
+    <style>
+    body {
+        background-color: #000000;
+        color: white; /* Opzionale: imposta il colore del testo per contrasto */
+    }
+    .stApp {
+        background-color: #000000; /* Assicura che anche l'area principale sia nera */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,)
+
+    st.title("La mia app con sfondo nero")
+    st.write("Questo testo dovrebbe essere bianco su sfondo nero.")
+
+
+
+
 
     st.balloons()
+    st.snow()
 
 if __name__ == "__main__":
     main()
